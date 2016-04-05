b8b7fbd16f9993a4d2fc378694fc2b914b912c8d
Michael Balarezo <mrezo@Michaels-MacBook-Pro-2.local>
2016-04-04 19:14:50 -0700

    Retrieves data from remote server using API

--- client/app/app.js
+++ client/app/app.js
@@ -1,1 +1,27 @@
-//main app module.
+angular.module('myGarageApp', [])
+  .controller('carController', function($scope, $http) {
+    $scope.$watch('search', function() {
+      fetch();
+    });
+
+    $scope.search = "Volkswagen";
+
+    function fetch() {
+      $http.get('http://api.edmunds.com/api/vehicle/v2/' + $scope.search +'/models?fmt=json&api_key=w3qpygvj3j3vvb8u8rppbbk2')
+        .then(function(response) {
+          $scope.details = response.data;
+          console.log($scope.details)
+        });
+
+      //render information to a view on the screen
+    }
+
+
+    // $scope.model = function(movie) {
+    //   $scope.search = movie.Title;
+    // };
+
+    // $scope.select = function() {
+    //   this.setSelectionRange(0, this.value.length);
+    // }
+  });
